c2fc2bb0502fcd97798396581dc816dcb5d03611
eneibaur <107266929+eneibaur@users.noreply.github.com>
2023-02-01 08:53:14 -0800

    Began working on modular javascript board

---
+++ script.js
@@ -0,0 +1,24 @@
+// Gameboard module: an object that has only one outward facing
+// component, the "add mark" component.
+
+(function() {
+    const gameBoard = {
+        startArray: ['x','x','x','x','x','x','x','x','x'],
+        init: function() {
+            this.cacheDom();
+            this.render();
+        }, 
+        cacheDom: function() {
+            this.board = document.querySelector('.board');
+        },
+        render: function() {
+            const data = {
+                board: this.startArray,
+            };
+            this.board.html('div');
+        },
+    }
+    gameBoard.init()
+})()
+
+
