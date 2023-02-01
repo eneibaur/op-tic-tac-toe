4f6a00a6610a176bcd5b795bf289f52a9e2e23f7
eneibaur <107266929+eneibaur@users.noreply.github.com>
2023-02-01 08:57:04 -0800

    Started on a seperate module format

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 // Gameboard module: an object that has only one outward facing
 // component, the "add mark" component.
 
-(function() {
-    const gameBoard = {
-        startArray: ['x','x','x','x','x','x','x','x','x'],
-        init: function() {
-            this.cacheDom();
-            this.render();
-        }, 
-        cacheDom: function() {
-            this.board = document.querySelector('.board');
-        },
-        render: function() {
-            const data = {
-                board: this.startArray,
-            };
-            this.board.html('div');
-        },
-    }
-    gameBoard.init()
-})()
+// (function() {
+//     const gameBoard = {
+//         startArray: ['x','x','x','x','x','x','x','x','x'],
+//         init: function() {
+//             this.cacheDom();
+//             this.render();
+//         }, 
+//         cacheDom: function() {
+//             this.board = document.querySelector('.board');
+//         },
+//         render: function() {
+//             const data = {
+//                 board: this.startArray,
+//             };
+//             this.board.html('div');
+//         },
+//     }
+//     gameBoard.init()
+// })()
 
+const gameBoard = (() => {
+    const board = document.querySelector('.board');
+    const render = () => {
+        board.appendChild('div');
+    }
+})();
 
