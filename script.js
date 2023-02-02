29a1cd719a5628a00b6096aadd65f94493db2356
eneibaur <107266929+eneibaur@users.noreply.github.com>
2023-02-02 08:39:53 -0800

    Each cell responds when clicked

--- script.js
+++ script.js
@@ -1,40 +1,37 @@
 // Gameboard module: an object that has only one outward facing
 // component, the "add mark" component.
 
-const gameBoard = {
-    boardArray: ['x','x','x','o','o','o','x','x','x'],
-    init: function() {
-        this.cacheDom();
-        this.render();
-    }, 
-    cacheDom: function() {
-        this.board = document.querySelector('.board');
-        this.cell = document.querySelector('#gamecell');
-    },
-    render: function() {
-        this.board.innerHtml = "";
-        this.boardArray.forEach(element => {
-            const clone = this.cell.content.cloneNode(true);
-            clone.querySelector('.piece').textContent = element;
-            this.board.append(clone);
-        });
-    },
-};
-
-gameBoard.init()
-
-// const gameBoard = (() => {
-//     const cacheDom = () => {
-//         const board = document.querySelector('.board');
-//         return {board};
-//     }
-//     const render = () => {
-//         cacheDom.board.appendChild('div');
-//     }
-//     const init = () => {
-//         cacheDom();
-//         render();
-//     }
-//     init();
-// })();
-
+const start = (() => {
+    const gameBoard = {
+        boardArray: ['x','x','x','o','o','o','x','x','x'],
+        init: function() {
+            this.cacheDom();
+            this.render();
+            this.bindEvents();
+        }, 
+        cacheDom: function() {
+            this.board = document.querySelector('.board');
+            this.cell = document.querySelector('#gamecell');
+        },
+        render: function() {
+            this.board.innerHtml = "";
+            this.boardArray.forEach(element => {
+                const clone = this.cell.content.cloneNode(true);
+                clone.querySelector('.piece').textContent = element;
+                this.board.append(clone);
+            });
+        },
+        bindEvents: function() {
+            const squares = document.querySelectorAll('.square');
+            squares.forEach(element => {
+                element.addEventListener('click', () => {
+                    this.placePiece();
+                });
+            });
+        },
+        placePiece: function() {
+            alert("clicked!");
+        }
+    };
+    gameBoard.init();
+})();
