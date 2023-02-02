28d75b911d0e75c64b65d5dd57de148bccbf6106
eneibaur <107266929+eneibaur@users.noreply.github.com>
2023-02-02 07:57:59 -0800

    Basic gameboard initializes

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
 // Gameboard module: an object that has only one outward facing
 // component, the "add mark" component.
 
-// (function() {
-//     const gameBoard = {
-//         startArray: ['x','x','x','x','x','x','x','x','x'],
-//         init: function() {
-//             this.cacheDom();
-//             this.render();
-//         }, 
-//         cacheDom: function() {
-//             this.board = document.querySelector('.board');
-//         },
-//         render: function() {
-//             const data = {
-//                 board: this.startArray,
-//             };
-//             this.board.html('div');
-//         },
-//     }
-//     gameBoard.init()
-// })()
+const gameBoard = {
+    boardArray: ['x','x','x','o','o','o','x','x','x'],
+    init: function() {
+        this.cacheDom();
+        this.render();
+    }, 
+    cacheDom: function() {
+        this.board = document.querySelector('.board');
+        this.cell = document.querySelector('#gamecell');
+    },
+    render: function() {
+        this.board.innerHtml = "";
+        this.boardArray.forEach(element => {
+            const clone = this.cell.content.cloneNode(true);
+            clone.querySelector('.piece').textContent = element;
+            this.board.append(clone);
+        });
+    },
+};
 
-const gameBoard = (() => {
-    const board = document.querySelector('.board');
-    const render = () => {
-        board.appendChild('div');
-    }
-})();
+gameBoard.init()
+
+// const gameBoard = (() => {
+//     const cacheDom = () => {
+//         const board = document.querySelector('.board');
+//         return {board};
+//     }
+//     const render = () => {
+//         cacheDom.board.appendChild('div');
+//     }
+//     const init = () => {
+//         cacheDom();
+//         render();
+//     }
+//     init();
+// })();
 
