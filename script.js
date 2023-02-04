aa0e86be556a4e1067b5dd1733308f1039298caf
eneibaur <107266929+eneibaur@users.noreply.github.com>
2023-02-04 08:35:44 -0800

    Rewrote code to match revealing module pattern

--- script.js
+++ script.js
@@ -1,58 +1,47 @@
 // Gameboard module: an object that has only one outward facing
 // component, the "add mark" component.
 
-const board = (() => {
-    const gameBoard = {
-        boardArray: ['','','','','','','','',''],
-        init: function() {
-            this.cacheDom();
-            this.render();
-            this.bindEvents();
-        }, 
+const gameBoard = (() => {
+    const boardArray = ['','','','','','','','',''];
 
-        cacheDom: function() {
-            this.board = document.querySelector('.board');
-            this.cell = document.querySelector('#gamecell');
-            // Make array for each square in the grid to log when it's clicked
-            this.boardPosition = [].slice.call(this.board.querySelectorAll('.square'), 0);
-        },
+    // cache DOM
+    let board = document.querySelector('.board');
+    let cell = document.querySelector('#gamecell');
+    let boardPosition = [].slice.call(board.querySelectorAll('.square'), 0);
+    
+    function render() {
+        clearBoard(board)
+        boardArray.forEach(element => {
+            const clone = cell.content.cloneNode(true);
+            clone.querySelector('.piece').textContent = element;
+            board.append(clone);
+        });
+        boardPosition = [].slice.call(board.querySelectorAll('.square'), 0);
+    }
 
-        render: function() {
-            this.clearBoard(this.board)
-            this.boardArray.forEach(element => {
-                const clone = this.cell.content.cloneNode(true);
-                clone.querySelector('.piece').textContent = element;
-                this.board.append(clone);
-            });
-            // To capture the boardPosition array (which does not exist until board is rendered)
-            // for future use
-            this.cacheDom();
-        },
-
-        bindEvents: function() {            
-            this.board.addEventListener('click', (e) => {
-                this.placePiece(e);
-            });
-        },
-
-        placePiece: function(e) {
-            const index = this.boardPosition.indexOf(e.target)
-            if (this.boardArray[index] === '') {
-                this.boardArray[index] = 'x'
-            } else {
-                return;
-            }
-            this.render();
-        },
-        
-        clearBoard: function(parent) {
-            while (parent.firstChild) {
-                parent.removeChild(parent.firstChild);
-            }
+    // bind events
+    board.addEventListener('click', (e) => {
+        placePiece(e);
+    });
+    
+    function placePiece(e) {
+        const index = boardPosition.indexOf(e.target)
+        if (boardArray[index] === '') {
+            boardArray[index] = 'x'
+        } else {
+            return;
         }
-        
+        render();
     };
-    gameBoard.init();
+
+    function clearBoard(parent) {
+        while (parent.firstChild) {
+            parent.removeChild(parent.firstChild);
+        }
+    }
+
+    render()
+
 })();
 
 const players = (() => {
@@ -77,5 +66,6 @@ const players = (() => {
     return {
         changePlayer: changePlayer
     };
+    
 })();
 
